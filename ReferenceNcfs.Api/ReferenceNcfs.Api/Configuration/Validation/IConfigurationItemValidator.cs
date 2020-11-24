ba6970d5e3a8e786ba0c3217f766f3d665c9d66e
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-11-24 17:07:57 +0000

    Add swagger

---
+++ ReferenceNcfs.Api/ReferenceNcfs.Api/Configuration/Validation/IConfigurationItemValidator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ReferenceNcfs.Api.Configuration.Validation.Errors;
+
+namespace ReferenceNcfs.Api.Configuration.Validation
+{
+    public interface IConfigurationItemValidator
+    {
+        bool TryParse(string key, string rawValue, List<ConfigurationParserError> validationErrors, out object parsed);
+    }
+}
--- ReferenceNcfs.Api/ReferenceNcfs.Api/Models/NcfsRequestModel.cs
+++ ReferenceNcfs.Api/ReferenceNcfs.Api/Models/NcfsRequestModel.cs
@@ -1,7 +1,10 @@
-﻿namespace ReferenceNcfs.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReferenceNcfs.Api.Models
 {
     public class NcfsRequestModel
     {
+        [Required]
         public string Base64Body { get; set; }
 
         public string DetectedFiletype { get; set; }
