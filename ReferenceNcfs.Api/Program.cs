5a9733e3c1d29781cab68dc255c29e4f7d6f3223
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-11-30 10:57:43 +0000

    Add Self-signed cert

--- ReferenceNcfs.Api/Program.cs
+++ ReferenceNcfs.Api/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +27,39 @@ namespace ReferenceNcfs.Api
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.AddServerHeader = false;
+                        options.ConfigureHttpsDefaults(opts =>
+                        {
+                            opts.ServerCertificate = LoadCertificate("/etc/ssl/certs/server.crt", "/etc/ssl/private/server.key");
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static X509Certificate2 LoadCertificate(string crtPath, string keyPath)
+        {
+            Console.WriteLine("Loading Self-Signed Certificate");
+
+            const string dashes = "-----";
+            var keyPem = File.ReadAllText(keyPath);
+            var index0 = keyPem.IndexOf(dashes, StringComparison.Ordinal);
+            var index1 = keyPem.IndexOf('\n', index0 + dashes.Length);
+            var index2 = keyPem.IndexOf(dashes, index1 + 1, StringComparison.Ordinal);
+            var keyDer = Convert.FromBase64String(keyPem.Substring(index1, index2 - index1));
+            X509Certificate2 certWithKey;
+
+            using (var certOnly = new X509Certificate2(crtPath))
+            using (var rsa = RSA.Create())
+            {
+                // For "BEGIN PRIVATE KEY"
+                rsa.ImportPkcs8PrivateKey(keyDer, out _);
+                certWithKey = certOnly.CopyWithPrivateKey(rsa);
+            }
+
+            Console.WriteLine("Using cert: " + certWithKey);
+            return certWithKey;
+        }
     }
 }
